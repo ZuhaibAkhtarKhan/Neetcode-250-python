f0476bfd5ba03e37d8c358b2eadd769aabd9e015
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-06-13 23:09:31 +0500

    2 easy

---
+++ Binary Search/35.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        h = len(nums) - 1
+        l = 0
+        while(l <= h):
+            
+            mid = (l+h)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                h = mid - 1
+            
+        return l
+
+        
+        
